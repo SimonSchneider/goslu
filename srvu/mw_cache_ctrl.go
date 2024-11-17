a612038f7a9d36e9cea4a9ddbc4621ea6e13fa84
SimonSchneider <dev@simonschneider.org>
2024-11-17 12:45:05 +0100

    add cache ctrl header mw

---
+++ srvu/mw_cache_ctrl.go
@@ -0,0 +1,22 @@
+package srvu
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func WithCacheHeader(ttl time.Duration) Middleware {
+	if ttl <= 0 {
+		panic("ttl must be greater than 0")
+	}
+	cacheControl := fmt.Sprintf("public, max-age=%d", int64(ttl.Seconds()))
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("serving %s", r.URL.Path)
+			w.Header().Set("Cache-Control", cacheControl)
+			h.ServeHTTP(w, r)
+		})
+	}
+}
